fcb3682f805c8fbebedd05f17b049ec9bc7414f8
aaharbaugh <aaronmharbaugh@gmail.com>
2019-03-18 23:00:11 -0500

    pushed content to new PostsPage component, to begin day 3 code

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,36 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import PostContainer  from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
-
-import data from "./dummy-data";
+import PostsPage from './components/PostContainer/PostsPage';
 
 class App extends Component {
   constructor(props){
     super();
 
-    this.state = {
-      data: [],
-
-    }
-  }
-
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({ data: data });
-    }, 1000);
   }
 
   render() {
     return (
-      <div className="App">
-            <SearchBar />
-            {this.state.data.length > 0 ? this.state.data.map(post => {
-                return <PostContainer data={post} key={post.timestamp}/>
-              }) : <br /> }
-
-      </div>
+      <PostsPage />
     );
   }
 }
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+import PostContainer  from "./PostContainer";
+import SearchBar from "../SearchBar/SearchBar";
+
+import data from "../../dummy-data";
+
+class PostsPage extends Component {
+  constructor(props){
+    super();
+
+    this.state = {
+      data: [],
+
+    }
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ data: data });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+            <SearchBar />
+            {this.state.data.length > 0 ? this.state.data.map(post => {
+                return <PostContainer data={post} key={post.timestamp}/>
+              }) : <br /> }
+
+      </div>
+    );
+  }
+}
+
+export default PostsPage;
+
